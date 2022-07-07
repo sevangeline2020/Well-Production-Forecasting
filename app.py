24a6c017362606859434d43de19abfe56b548aae
sevangeline2020 <92109516+sevangeline2020@users.noreply.github.com>
2022-07-07 22:30:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ st.subheader('This project aims at replacing the traditional DCA and discusses t
 
 st.header('**SAMPLE DATASET**')
 column = ['Month','Production_rate']
-data = pd.read_csv(r"C:\Users\Dell\Downloads\ProductionData2.xlsx - Sheet1.csv", names = column);
+data = pd.read_csv("ProductionData2.xlsx - Sheet1.csv", names = column);
 st.table(data)
 
 
 st.header('**DATETIME ANALYSIS**')
 dateparse = lambda dates: pd.datetime.strptime(dates, '%m/%d/%Y')
-data = pd.read_csv(r"C:\Users\Dell\Downloads\ProductionData2.xlsx - Sheet1.csv",names = column, parse_dates=['Month'], index_col=['Month'],date_parser=dateparse);
+data = pd.read_csv("ProductionData2.xlsx - Sheet1.csv",names = column, parse_dates=['Month'], index_col=['Month'],date_parser=dateparse);
 st.line_chart(data)
 st.pyplot()
 
