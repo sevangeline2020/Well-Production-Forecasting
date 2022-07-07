2b77c32fb58b863539b5aed69d6052984d070812
sevangeline2020 <92109516+sevangeline2020@users.noreply.github.com>
2022-07-07 21:38:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,14 +33,14 @@ st.header('**PROBLEM STATEMENT**')
 st.subheader('This project aims at replacing the traditional DCA and discusses the application of the widely accepted concepts of time series analysis for forecasting well production data by analyzing statistical trends from historical data.')
 
 st.header('**SAMPLE DATASET**')
-column =['Month','Production_rate']
-data = pd.read_csv("ProductionData2.xlsx - Sheet1.csv', names = column);
+column = ['Month','Production_rate']
+data = pd.read_csv("C:\Users\Dell\Downloads\ProductionData2.xlsx - Sheet1.csv", names = column);
 st.table(data)
 
 
 st.header('**DATETIME ANALYSIS**')
 dateparse = lambda dates: pd.datetime.strptime(dates, '%m/%d/%Y')
-data = pd.read_csv("ProductionData2.xlsx - Sheet1.csv',names = column, parse_dates=['Month'], index_col=['Month'],date_parser=dateparse);
+data = pd.read_csv("C:\Users\Dell\Downloads\ProductionData2.xlsx - Sheet1.csv",names = column, parse_dates=['Month'], index_col=['Month'],date_parser=dateparse);
 st.line_chart(data)
 st.pyplot()
 
