938671d5aa8555b9190595685a5369f1c926e2ff
sevangeline2020 <92109516+sevangeline2020@users.noreply.github.com>
2022-07-11 17:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,6 @@ st.write("""Mean absolute error = 67.58
 
 st.write("**Thank You**" '🙏')
 st.write("To get the source code click on the [link](https://colab.research.google.com/drive/1bh9Nu39j00z6agm_wZZ1S5Rx7gJJPca4?usp=sharing#scrollTo=voiXSbodxON_)")
-  
+st.write("@contact[link](sevangeline2020@gmail.com)")
 
 
